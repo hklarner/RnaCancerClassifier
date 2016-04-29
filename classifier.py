181a0d0180e41a6466e3c84c90947eed7d07bd10
katinkab <katinka.becker@fu-berlin.de>
2016-04-29 12:25:11 +0200

    added mat2csv as function

--- classifier.py
+++ classifier.py
@@ -4,6 +4,8 @@
 
 import csv
 import subprocess
+from scipy.io import loadmat
+import numpy as np
 
 
 
@@ -386,7 +388,112 @@ def check_classifier(FnameCSV, GateInputs):
         print " result = %i inconsistencies"%(len(hits)), hits
     else:
         print " result = classifier and data are consistent"
+        
+        
+       
+       
+       
+def mat2csv(FnameMAT, Threshold):
+	"""
+   Threshold for binarization (0 if variable<250 else 1):
+   use 250 for toy.mat files
+   """
+	print "\n--- mat2csv"
+   # Read .mat file
+	datafile = loadmat(FnameMAT)
+	print " input file: "+str(FnameMAT)
 
+	col = datafile['SimDataHeike']['Values'][0,0][0,:]
+	row = datafile['SimDataHeike']['Values'][0,0][:,0]
+	print " samples: "+str(col.size)
+	print " miRNAs: "+str(row.size)
+
+	# ID column
+	id_column = []
+	id_column.append("ID")
+	for i in range(0,col.size):
+		id = i
+		id_column.append(i+1)
+
+	# Write .csv file
+	np.savetxt(str(FnameMAT[:-4])+'_binary.csv', 
+          	 id_column, 
+          	 delimiter=',', 
+          	 fmt='%s')
+           
+	# Annots column
+	annots_column = []
+	annots_column.append("Annots")
+	cancer_count = 0
+	for c in range(0,col.size):
+		variable = datafile['SimDataHeike']['Annots'][0,0][0,c]
+		annots_column.append(variable)
+		if variable == 1:
+			cancer_count=cancer_count+1
+	print " cancer cells: "+str(cancer_count)
+	
+	oldfile = open(str(FnameMAT[:-4])+'_binary.csv')
+	olddata = [item for item in csv.reader(oldfile)]
+	oldfile.close()    
+
+	newdata = []
+ 
+	for i, item in enumerate(olddata):
+    		try:
+      	 		item.append(annots_column[i])
+    		except IndexError, e:
+       	 		item.append("placeholder")
+    		newdata.append(item)
+ 
+	newfile = open(str(FnameMAT[:-4])+'_binary.csv', 'w')
+	csv.writer(newfile).writerows(newdata)
+	newfile.close()
+	
+	# Values of miRNAs in columns            
+	# First row to end
+	for r in range(0,row.size):      
+           
+		oldfile = open(str(FnameMAT[:-4])+'_binary.csv')
+		olddata = [item for item in csv.reader(oldfile)]
+		oldfile.close()    
+      
+		newcolumn = []
+		newcolumn.append("g"+str(r+1))
+		for c in range(0,col.size):
+				variable = datafile['SimDataHeike']['Values'][0,0][r,c]
+				binary = 0 if variable<250 else 1
+				newcolumn.append(binary)
+ 
+				newdata = []
+ 
+		for i, item in enumerate(olddata):
+    			try:
+       				item.append(newcolumn[i])
+    			except IndexError, e:
+        				item.append("placeholder")
+    			newdata.append(item)
+ 
+		newfile = open(str(FnameMAT[:-4])+'_binary.csv', 'w')
+		csv.writer(newfile).writerows(newdata)
+		newfile.close()
+	
+	# Get classifier
+	print " classifier: "
+	for c in range(0,6):
+		input = datafile['SimDataHeike']['Synthesis_Function'][0,0][0,c]
+		for r in range(0,3):
+			if input[r] > 0:
+				print "   gate_input("+str(c+1)+",positive,"+str(input[r])+")"
+			if input[r] < 0:
+				nosign = input[r]*-1
+				print "   gate_input("+str(c+1)+",negative,"+str(nosign)+")"
+				
+	print " created: "+str(FnameMAT[:-4])+"_binary.csv"
+
+
+ 
+ 
+ 
  
 
 if __name__=="__main__":
