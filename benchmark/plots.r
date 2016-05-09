bfaf9764ec81bae81989df692072545173fac14a
katinkab <katinka.becker@fu-berlin.de>
2016-05-09 12:10:36 +0200

    new plot + fix 3D plots

--- benchmark/plots.r
+++ benchmark/plots.r
@@ -12,11 +12,11 @@ time_first <- data[4]
 time_all <- data[5]
 solutions <- data[6]
 
-print(max(cols))
 
 #start plot axes in (0,0)
 par(xaxs="i", yaxs="i") 
 par(mgp=c(3,1,0))
+
 
 #rows - columns - time_first
 wireframe(time_first ~ cols * rows, data=no_na_data, main = "Generating first model", drape = TRUE, perspective=TRUE, colorkey=FALSE, xlab=list("number of columns", rot=35), ylab=list("number of rows", rot=-40), zlab="time (s)", xlim=c(min(cols),max(cols)), ylim=c(min(rows),max(rows)), zlim=c(min(time_first),max(time_first)), scales = list(arrows=FALSE,font = 0.3,tck = c(0.5, 0.5, 0.5),distance =c(2, 2, 2)),zoom = 0.6, cex.lab=0.05)
@@ -32,7 +32,12 @@ update(p[rep(1, prod(npanel))], layout = npanel,
         panel.wireframe(..., screen = list(z = rotz[current.column()],
                                            x = rotx[current.row()]))})
 
+
 #time for calculating all solutions - number of solutions
-plot(time_all ~ solutions, data = no_na_data, main = "Generating all models", type="l", col="blue", xlab="number of solutions", ylab="time (s)", xlim=c(min(solutions),max(solutions)), ylim=c(min(time_all),max(time_all)), xaxp  = c(0,round(max(solutions), digits=-2), 20), yaxp  = c(0,round(max(time_all), digits=-2), 20), lwd=1.5)
+plot(time_all ~ solutions, data = no_na_data, main = "Generating all models", type="p", col="blue", xlab="number of solutions", ylab="time (s)", xlim=c(min(solutions),100000), ylim=c(min(time_all),20000), xaxp  = c(0,100000, 20), yaxp  = c(0,20000, 20), lwd=1.5)
+
+
+#time for calculating first model - number of cols (number of rows fixed to 10)
+plot(time_first ~ cols, data = no_na_data, main = "Generating first model (number of rows = 10)", type="p", col="blue", xlab="number of columns", ylab="time (s)", xlim=c(min(cols),max(cols)), ylim=c(min(time_first),max(time_first)), xaxp  = c(0,max(cols), 20), yaxp  = c(0,round(max(time_all), digits=-2), 20), lwd=1.5)
 
 
