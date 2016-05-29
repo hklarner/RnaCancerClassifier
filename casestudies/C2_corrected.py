2a956b7e6c983b5804944727a83b0ff9a65f1523
hklarner <leevilux@yahoo.de>
2016-05-29 17:46:31 +0200

    update: parameter BreakSymmetries

--- casestudies/C2_corrected.py
+++ casestudies/C2_corrected.py
@@ -13,30 +13,8 @@ GateTypes = [{"LowerBoundPos":0,"UpperBoundPos":3,
               "UpperBoundOcc":4}]
 EfficiencyConstraint = True
 OptimizationStrategy = 1
+BreakSymmetries = True
 
-"""
-User input explained:
-
- FnameCSV              = CSV data file (0/1 matrix, tissue samples = rows)
-  Header               = ID, Annots, g1, g2, g3, ... (ID:sampleid, Annots:0=healthy/1=cancer, g1:0=low expression/1=high expression)
- FnameASP              = ASP filename that is generated
- UpperBoundInputs      = upper bound for total number of inputs for classifier
- UpperBoundGates       = upper bount for number of gates
- GateTypes             = a gate type is defined in terms of
-   LowerBoundPos = lower bound of positive inputs to gate
-   UpperBoundPos = upper bound of positive inputs to gate
-   LowerBoundNeg = lower bound of negative inputs to gate
-   UpperBoundNeg = upper bound of negative inputs to gate
-   UpperBoundOcc = upper bound of occurences of gate in classifier
- EfficiencyConstraint  = Katinka's efficiency constraints: positive (negative) inputs must be highly (lowly) expressed on some cancer tissue
- OptimizationStrategy  = 1..4
-   1 = minimize number of gates, then minimize number of inputs
-   2 = minimize number of inputs, then minimize number of gates
-   3 = minimize number of inputs
-   4 = minimize number of gates
-
- Note: a classifier is the conjunction of disjunctive gates (CNF)
-"""
 
 import sys
 sys.path = ["../"] + sys.path
@@ -51,7 +29,8 @@ if __name__=="__main__":
             UpperBoundGates,
             GateTypes,
             EfficiencyConstraint,
-            OptimizationStrategy)
+            OptimizationStrategy,
+            BreakSymmetries)
             
     if 0 :
         GateInputs = "gate_input(1,negative,g7) gate_input(2,negative,g6) gate_input(3,negative,g4) gate_input(4,negative,g3) "
--- casestudies/symmetries.py
+++ casestudies/symmetries.py
@@ -15,29 +15,6 @@ EfficiencyConstraint = True
 OptimizationStrategy = 2
 BreakSymmetries = True
 
-"""
-User input explained:
-
- FnameCSV              = CSV data file (0/1 matrix, tissue samples = rows)
-  Header               = ID, Annots, g1, g2, g3, ... (ID:sampleid, Annots:0=healthy/1=cancer, g1:0=low expression/1=high expression)
- FnameASP              = ASP filename that is generated
- UpperBoundInputs      = upper bound for total number of inputs for classifier
- UpperBoundGates       = upper bount for number of gates
- GateTypes             = a gate type is defined in terms of
-   LowerBoundPos = lower bound of positive inputs to gate
-   UpperBoundPos = upper bound of positive inputs to gate
-   LowerBoundNeg = lower bound of negative inputs to gate
-   UpperBoundNeg = upper bound of negative inputs to gate
-   UpperBoundOcc = upper bound of occurences of gate in classifier
- EfficiencyConstraint  = Katinka's efficiency constraints: positive (negative) inputs must be highly (lowly) expressed on some cancer tissue
- OptimizationStrategy  = 1..4
-   1 = minimize number of gates, then minimize number of inputs
-   2 = minimize number of inputs, then minimize number of gates
-   3 = minimize number of inputs
-   4 = minimize number of gates
-
- Note: a classifier is the conjunction of disjunctive gates (CNF)
-"""
 
 import sys
 sys.path = ["../"] + sys.path
