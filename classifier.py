38dd216082c761850d04f09f632b4b4efcc33aae
HannesK <hannes.klarner@fu-berlin.de>
2016-05-30 11:04:17 +0200

    update ASP format

--- classifier.py
+++ classifier.py
@@ -104,22 +104,20 @@ def csv2asp(FnameCSV,
         print " miRNAs: ", len(miRNAs)
         print " samples:", len(rows)
 
-    datafile = [""]
-    datafile+= ["% ASP constraints for computing a classifier (in this case a Boolean Function),"]
-    datafile+= ["% that agrees with given tissue data (either cancerous or healthy) and"]
-    datafile+= ["% and satisfies certain structural constraints."]
-    datafile+= ["% Note: a classifier is the conjunction of disjunctive gates (CNF)"]
-    datafile+= ["% written by K. Becker and H. Klarner, March 2016, FU Berlin."]
-    datafile+= [""]
-    datafile+= ["%% InputFile = %s"%FnameCSV]
-    datafile+= ["%%  upper bound on inputs: %i"%UpperBoundInputs]
-    datafile+= ["%%  upper bound on gates: %i"%UpperBoundGates]
-    datafile+= ["%%  gate types: %s"%str(GateTypes)]
-    datafile+= ["%%  efficiency constraints: %s"%str(EfficiencyConstraint)]
-    datafile+= ["%%  optimization strategy: %i  (%s)"%(OptimizationStrategy,OptimizationStrategyMapping[OptimizationStrategy])]
-    datafile+= [""]
+    datafile = ['']
+    datafile+= ['% ASP constraints for computing a miRNA cancer classifier']
+    datafile+= ['% that agrees with given tissue data and satisfies given structural constraints.']
+    datafile+= ['% note: A classifier is a Boolean expression in conjunctive form.']
+    datafile+= ['% the homepage of the project is https://github.com/hklarner/RnaCancerClassifier.']
+    datafile+= ['% written by K. Becker and H. Klarner, March 2016, FU Berlin.']
+    datafile+= ['']
+    datafile+= ['%% InputFile = %s'%FnameCSV]
+    datafile+= ['%%  efficiency constraints: %s'%str(EfficiencyConstraint)]
+    datafile+= ['%%  optimization strategy: %i  (%s)'%(OptimizationStrategy,OptimizationStrategyMapping[OptimizationStrategy])]
+    datafile+= ['']
+    datafile+= ['']
                 
-    datafile+= ['% the tissue data, a tissue ID followed by either "cancer" or "healthy"']
+    datafile+= ['%%% The tissue data']
     dummy = []
     for x in rows:
         y = "healthy" if x["Annots"]=="0" else "cancer"
@@ -130,12 +128,7 @@ def csv2asp(FnameCSV,
                 
     datafile+= [" ".join(dummy)]
     datafile+= [""]
-
-    datafile+= ['% for binding variables we need the "is_tissue_id" predicate',
-                "is_tissue_id(X) :- tissue(X,Y).",
-                ""]
-
-    datafile+= ['% the miRNA data, a tissue ID and miRNA ID followed by either "high" or "low"']
+    datafile+= ['%%% The miRNA data']
     dummy = []
     for x in rows:
         for miRNA in miRNAs:
@@ -145,20 +138,19 @@ def csv2asp(FnameCSV,
                 datafile+= [" ".join(dummy)]
                 dummy = []
                 
-    datafile+= [" ".join(dummy)]
-    datafile+= [""]
-
-    datafile+= ['% for binding variables we need the "is_miRNA" predicate',
-                "is_mirna(Y) :- data(X,Y,Z).",
-                ""]
-
+    datafile+= [' '.join(dummy)]
     datafile+= ['']
     datafile+= ['']
-    datafile+= ['%%%% Classifier Structure %%%%']
-    datafile+= ["% definition of gate types in terms of upper bounds on number of inputs"]
+    
+    datafile+= ["%%% User Input"]
+    datafile+= ['lower_bound_inputs(%i).'%LowerBoundInputs]
+    datafile+= ['upper_bound_inputs(%i).'%UpperBoundInputs]
+    datafile+= ['lower_bound_gates(%i).'%LowerBoundGates]
+    datafile+= ['upper_bound_gates(%i).'%UpperBoundGates]
+    datafile+= ['']
 
     for x, gate_type in enumerate(GateTypes):
-        datafile+= ["%% constraints for gate type %i."%(x+1)]
+        datafile+= ["%% gate type %i."%(x+1)]
         datafile+= ["is_gate_type(%i)."%(x+1)]
         datafile+= ["upper_bound_pos_inputs(%i, %i)."%(x+1,gate_type["UpperBoundPos"])]
         datafile+= ["upper_bound_neg_inputs(%i, %i)."%(x+1,gate_type["UpperBoundNeg"])]
@@ -167,109 +159,102 @@ def csv2asp(FnameCSV,
         datafile+= ["upper_bound_gate_occurence(%i, %i)."%(x+1,gate_type["UpperBoundOcc"])]
         datafile+= ['']
 
-    datafile+= ['']
-    datafile+= ["% each input may be positive or negative"]
+    datafile+= ['% binding of variables']
+    datafile+= ["is_tissue_id(X) :- tissue(X,Y)."]
+    datafile+= ["is_mirna(Y) :- data(X,Y,Z)."]
     datafile+= ['is_sign(positive). is_sign(negative).']
-    datafile+= [""]
-    datafile+= ["% bounds for total number of inputs"]
-    datafile+= ['lower_bound_inputs(%i).'%LowerBoundInputs]
-    datafile+= ['upper_bound_inputs(%i).'%UpperBoundInputs]
-
-    datafile+= [""]
-    datafile+= ["% bounds for total number of gates"]
-    datafile+= ['lower_bound_gates(%i).'%LowerBoundGates]
-    datafile+= ['upper_bound_gates(%i).'%UpperBoundGates]
-        
     datafile+= ['']
-    datafile+= ['']
-    datafile+= ['%%%% Decisions %%%%']
-    datafile+= ['% First decision: the exact number of gates']
+    
+    datafile+= ['%%% Constraints']
+    datafile+= ['% number of gates']
     datafile+= ['1 {number_of_gates(X..Y)} 1 :- lower_bound_gates(X), upper_bound_gates(Y).']
     datafile+= ['is_gate_id(1..X) :- number_of_gates(X).']
     datafile+= ['']
-    datafile+= ['% Second decision: each gates is assigned a gate type']
+    
+    datafile+= ['% assignment of gate types']
     datafile+= ['1 {gate_type(GateID, X): is_gate_type(X)} 1 :- is_gate_id(GateID).']
     datafile+= ['']
 
     if EfficiencyConstraint:
-        datafile+= ['% efficiency ON: restrict miRNA for inputs (requires the assumption that number of miRNAs is minimal)']
+        datafile+= ['% inputs for gates (EfficiencyConstraint=True)']
         datafile+= ['feasible_pos_miRNA(MiRNA) :- data(TissueID, MiRNA, high), tissue(TissueID,cancer).']
         datafile+= ['feasible_neg_miRNA(MiRNA) :- data(TissueID, MiRNA, low),  tissue(TissueID,cancer).']
         datafile+= ['feasible_pos_miRNA(MiRNA) :- gate_input(GateID, positive, MiRNA).']
         datafile+= ['feasible_neg_miRNA(MiRNA) :- gate_input(GateID, negative, MiRNA).']
-        
         datafile+= ['']
-        datafile+= ['% Third decision: each gate is assigned a number of inputs']
         datafile+= ['X {gate_input(GateID, positive, MiRNA): feasible_pos_miRNA(MiRNA)} Y :- gate_type(GateID, GateType), lower_bound_pos_inputs(GateType, X), upper_bound_pos_inputs(GateType, Y).']
         datafile+= ['X {gate_input(GateID, negative, MiRNA): feasible_neg_miRNA(MiRNA)} Y :- gate_type(GateID, GateType), lower_bound_neg_inputs(GateType, X), upper_bound_neg_inputs(GateType, Y).']
+        datafile+= ['']
         
     else:
-        datafile+= ['% efficiency OFF: unrestricted miRNAs for inputs']
-        datafile+= ['% Third decision: each gate is assigned a number of inputs']
+        datafile+= ['% inputs for gates (EfficiencyConstraint=False']
         datafile+= ['X {gate_input(GateID, positive, MiRNA): is_mirna(MiRNA)} Y :- gate_type(GateID, GateType), lower_bound_pos_inputs(GateType, X), upper_bound_pos_inputs(GateType, Y).']
         datafile+= ['X {gate_input(GateID, negative, MiRNA): is_mirna(MiRNA)} Y :- gate_type(GateID, GateType), lower_bound_neg_inputs(GateType, X), upper_bound_neg_inputs(GateType, Y).']        
-        
+        datafile+= ['']
     
+    
+    datafile+= ['% at least one input for each gate']
+    datafile+= ['1 {gate_input(GateID,Sign,MiRNA): is_sign(Sign), is_mirna(MiRNA)} :- is_gate_id(GateID).']
     datafile+= ['']
-    datafile+= ['']
-    datafile+= ['%%%% Constraints %%%%']
-    datafile+= ['% each gate must have at least one input']
-    datafile+= ['1 {gate_input(GateID, Sign, MiRNA): is_sign(Sign), is_mirna(MiRNA)} :- is_gate_id(GateID).']
 
-    datafile+= ['']
-    datafile+= ['% inputs must be unique for a classifer']
+    datafile+= ['% inputs must be unique']
     datafile+= ["{gate_input(GateID,Sign,MiRNA): is_sign(Sign), is_gate_id(GateID)} 1 :- is_mirna(MiRNA)."]
+    datafile+= ['']
     
+    datafile+= ['% number of inputs is bounded']
+    datafile+= ['X {gate_input(GateID,Sign,MiRNA): is_gate_id(GateID), is_sign(Sign), is_mirna(MiRNA)} Y :- lower_bound_inputs(X), upper_bound_inputs(Y).']    
     datafile+= ['']
-    datafile+= ['% the total number of inputs is bounded']
-    datafile+= ['X {gate_input(GateID,Sign,MiRNA): is_gate_id(GateID), is_sign(Sign), is_mirna(MiRNA)} Y :- lower_bound_inputs(X), upper_bound_inputs(Y).']
-        
-    datafile+= ['']
-    datafile+= ['% the number of occurences of a gate type is bounded']
+    
+    datafile+= ['% occurences of gate types is bounded']
     datafile+= ['{gate_type(GateID,GateType): is_gate_id(GateID)} X :- upper_bound_gate_occurence(GateType,X).']
+    datafile+= ['']
+    
+    datafile+= ['% gates fire condition']
+    datafile+= ["gate_fires(GateID,TissueID) :- gate_input(GateID,positive,MiRNA), data(TissueID,MiRNA,high)."]
+    datafile+= ["gate_fires(GateID,TissueID) :- gate_input(GateID,negative,MiRNA), data(TissueID,MiRNA,low)."]
+    datafile+= ['']
+    
+    datafile+= ['% prediction of classifier']
+    datafile+= ["classifier(TissueID,healthy) :- not gate_fires(GateID, TissueID), is_gate_id(GateID), is_tissue_id(TissueID)."]
+    datafile+= ["classifier(TissueID,cancer) :- not classifier(TissueID, healthy), is_tissue_id(TissueID)."]
+    datafile+= ['']
+    
+    datafile+= ['% consistency of classifier and data']
+    datafile+= [':- tissue(TissueID,healthy), classifier(TissueID,cancer).']
+    datafile+= [':- tissue(TissueID,cancer),  classifier(TissueID,healthy).']
+    datafile+= ['']
 
     if BreakSymmetries:
         datafile+= ['']
-        datafile+= ['% breaking symmetries']
-        datafile+= ['% gate ids are assigned to the smallest possible types']
+        datafile+= ['%%% Breaking symmetries']
+        datafile+= ['% gate id symmetries']
         datafile+= ['GateType1 <= GateType2 :- gate_type(GateID1, GateType1), gate_type(GateID2, GateType2), GateID1 <= GateID2.']
+        datafile+= ['']
+        datafile+= ['% gate input symmetries']            
         datafile+= ['MiRNA1<=MiRNA2 :- gate_type(GateID1, GateType), gate_type(GateID2, GateType), gate_input(GateID1,Sign,MiRNA1), gate_input(GateID2,Sign,MiRNA2), GateID1<=GateID2.']
-
-    datafile+= ['']
-    datafile+= ['% gates are disjunctive (one active input suffices to activate gate)']
-    datafile+= ["gate_fires(GateID,TissueID) :- gate_input(GateID,positive,MiRNA), data(TissueID,MiRNA,high)."]
-    datafile+= ["gate_fires(GateID,TissueID) :- gate_input(GateID,negative,MiRNA), data(TissueID,MiRNA,low)."]
-
-    datafile+= ['']
-    datafile+= ['% the classifier is a conjunction of all gate evaluations.']
-    datafile+= ["classifier(TissueID,healthy) :- not gate_fires(GateID, TissueID), is_gate_id(GateID), is_tissue_id(TissueID)."]
-    datafile+= ["classifier(TissueID,cancer) :- not classifier(TissueID, healthy), is_tissue_id(TissueID)."]
-
-    datafile+= ['']
-    datafile+= ["% the classifier must agree with the tissue data."]
-    datafile+= [":- tissue(TissueID,healthy), classifier(TissueID,cancer)."]
-    datafile+= [":- tissue(TissueID,cancer),  classifier(TissueID,healthy)."]
-    
-    datafile+= ['']
+        datafile+= ['']
+        datafile+= ['']
+        
     if OptimizationStrategy==1:
-        datafile+= ["% optimization setup 2: first number of inputs then number of gates."]
-        datafile+= ["#minimize{ 1@1,MiRNA: gate_input(GateID,Sign,MiRNA) }."]
-        datafile+= ["#minimize{ 1@2,GateID:gate_input(GateID,Sign,MiRNA) }."]
+        datafile+= ['% optimization setup 2: first number of inputs then number of gates.']
+        datafile+= ['#minimize{ 1@1,MiRNA: gate_input(GateID,Sign,MiRNA) }.']
+        datafile+= ['#minimize{ 1@2,GateID:gate_input(GateID,Sign,MiRNA) }.']
         
     elif OptimizationStrategy==2:
-        datafile+= ["% optimization setup 1: first number of gates then number of inputs."]
-        datafile+= ["#minimize{ 1@1,GateID:gate_input(GateID,Sign,MiRNA) }."]
-        datafile+= ["#minimize{ 1@2,MiRNA: gate_input(GateID,Sign,MiRNA) }."]
+        datafile+= ['% optimization setup 1: first number of gates then number of inputs.']
+        datafile+= ['#minimize{ 1@1,GateID:gate_input(GateID,Sign,MiRNA) }.']
+        datafile+= ['#minimize{ 1@2,MiRNA: gate_input(GateID,Sign,MiRNA) }.']
         
     elif OptimizationStrategy==3:
-        datafile+= ["% optimization setup 3: only number of inputs."]
-        datafile+= ["#minimize{ 1,MiRNA:gate_input(GateID,Sign,MiRNA) }."]
+        datafile+= ['% optimization setup 3: only number of inputs.']
+        datafile+= ['#minimize{ 1,MiRNA:gate_input(GateID,Sign,MiRNA) }.']
         
     elif OptimizationStrategy==4:
-        datafile+= ["% optimization setup 4: only number of gates."]
-        datafile+= ["#minimize{ 1,GateID:gate_input(GateID,Sign,MiRNA) }."]
+        datafile+= ['% optimization setup 4: only number of gates.']
+        datafile+= ['#minimize{ 1,GateID:gate_input(GateID,Sign,MiRNA) }.']
+                    
     elif OptimizationStrategy==0:
-        datafile+= ["% no optimization selected"]
+        datafile+= ['% no optimization selected']
     
     datafile+= ['']
     datafile+= ["#show gate_input/3."]
@@ -614,10 +599,12 @@ def pilot(Parameters, FnamePaths=None):
     Parameters["FnameASP"] = None
     aspfile = csv2asp(**Parameters)
 
+    cmd_gringo = [CMD_GRINGO]
+    cmd_clasp  = [CMD_CLASP] + params_clasp
+
     try:
-        cmd_gringo = [CMD_GRINGO]
+        
         proc_gringo = subprocess.Popen(cmd_gringo, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        cmd_clasp  = [CMD_CLASP] + params_clasp
         proc_clasp  = subprocess.Popen(cmd_clasp,  stdin=proc_gringo.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         proc_gringo.stdin.write( aspfile )
@@ -627,17 +614,17 @@ def pilot(Parameters, FnamePaths=None):
 
     except Exception as Ex:
         print Ex
-        msg = "\nCall to gringo and / or clasp failed."
-        if FnameASP!=None:
-            msg+= '\ncommand: "%s"'%' '.join(cmd_gringo+['|']+cmd_clasp)
+        msg = '\n!!Call to gringo and / or clasp failed.'
+        msg+= '\n!!command: "%s"'%' '.join(cmd_gringo+['|']+cmd_clasp)
+        msg+= '\n!!Check %s\n'%FnamePaths
         print msg
         raise Ex
 
     if error:
         print error
-        msg = "\nCall to gringo and / or clasp failed."
-        if FnameASP!=None:
-            msg+= '\ncommand: "%s"'%' '.join(cmd_gringo+['|']+cmd_clasp)
+        msg = '\n!!Call to gringo and / or clasp failed.'
+        msg+= '\n!!command: "%s"'%' '.join(cmd_gringo+['|']+cmd_clasp)
+        msg+= '\n!!Check %s\n'%FnamePaths
         print msg
         raise Exception
 
