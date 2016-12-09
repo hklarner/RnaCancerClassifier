c5528aadfafbc93aabeb4b9e14845d6614e04925
HannesK <hannes.klarner@fu-berlin.de>
2016-12-09 15:06:01 +0100

    updates

---
+++ benchmarks/templates/crossvalidations/leave-one-out.py
@@ -0,0 +1,26 @@
+
+
+import interfaces
+
+
+def generator(Matrix):
+    for x in range(len(Matrix)):
+        new_matrix = [row for y,row in enumerate(Matrix) if x!=y]
+        row = Matrix[x]
+        
+        yield new_matrix, row
+        
+        
+
+def get_error(FunctionAnnotation, FunctionSolution, TestData):
+    row = interfaces.files.row2dict(TestData)
+
+    if FunctionAnnotation(row)==FunctionSolution(row):
+        return 0
+
+    return 1
+
+
+def get_generalization_error(TotalError, Matrix):
+
+    return 1.*TotalError / len(Matrix)
